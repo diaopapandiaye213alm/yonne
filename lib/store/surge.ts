41f8c5eb0d292fbbba12df3bca4abcf616d03256
Papa Ndiaye Diao <diaopapandiaye213alm@gmail.com>
2026-05-07 03:20:42 +0000

    feat(admin): csv util and surge store

---
+++ lib/store/surge.ts
@@ -0,0 +1,16 @@
+"use client";
+import { create } from "zustand";
+
+interface SurgeState {
+  multiplier: number;
+  autoMode: boolean;
+  setMultiplier: (v: number) => void;
+  toggleAutoMode: () => void;
+}
+
+export const useSurgeStore = create<SurgeState>((set) => ({
+  multiplier: 1.4,
+  autoMode: true,
+  setMultiplier: (v) => set({ multiplier: Math.round(v * 10) / 10 }),
+  toggleAutoMode: () => set((s) => ({ autoMode: !s.autoMode })),
+}));
